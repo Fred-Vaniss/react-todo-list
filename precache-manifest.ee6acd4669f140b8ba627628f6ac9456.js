9cafab52080d57f28ac9773f94c865eb2ff80c37
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-08-07 20:15:01 +0200

    Updates

---
+++ precache-manifest.ee6acd4669f140b8ba627628f6ac9456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3925cb79098a100ce2818d544dc846f8",
+    "url": "/Fred-Vaniss/react-todo-list/index.html"
+  },
+  {
+    "revision": "a51545e8beb3ee727e3d",
+    "url": "/Fred-Vaniss/react-todo-list/static/css/main.6682083f.chunk.css"
+  },
+  {
+    "revision": "fb2d10cdac24e8427c27",
+    "url": "/Fred-Vaniss/react-todo-list/static/js/2.d5b93b5d.chunk.js"
+  },
+  {
+    "revision": "a51545e8beb3ee727e3d",
+    "url": "/Fred-Vaniss/react-todo-list/static/js/main.c0956d4e.chunk.js"
+  },
+  {
+    "revision": "8e9cbf74a92aa3b6266d",
+    "url": "/Fred-Vaniss/react-todo-list/static/js/runtime~main.ed4430b9.js"
+  }
+]);
