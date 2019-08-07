84f2e51b955044df82e885666d9d77a8f2070b40
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-08-07 10:33:39 +0200

    Suppression des fichiers inutiles. Ajout du component de base avec boutton check.

---
+++ src/components/TodoItem/TodoItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class TodoItem extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state={
+            checked: false
+        }
+    }
+
+    checkItem = () => {
+        if (!this.state.checked){
+            this.setState({checked: true})
+        } else {
+            this.setState({checked: false})
+        }
+    }
+
+    render(){
+        return(
+            <div className='todo-item'>
+                <span className='check' role="img" aria-label="check" onClick={this.checkItem}>{this.state.checked ? "✅" : "🟥"}</span> { this.props.task }
+            </div>
+        )
+    }
+}
+
+export default TodoItem
