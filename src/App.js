f6a9ede64c8e181905683261c69a09cd6d76ab90
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-08-07 19:44:14 +0200

    Ajout de commentaires

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+///////////////////////////////////////////
+//                                       //
+//  Todolist par Frédérick Van Isschot   //
+//  https://github.com/Fred-Vaniss       //
+//                                       //
+///////////////////////////////////////////
+
 import React from 'react';
-// import ls from 'local-storage'
 import { TodoItem } from './components'
 import { isNull } from 'util';
 
@@ -7,14 +13,15 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      userInput: '',
-      todoIndex: 0,
-      todoList: []
+      userInput: '',  // Champ de texte
+      todoIndex: 0,   // Indexe de nouvelles entrées
+      todoList: []    // Liste des tâches
     }
-    this.handleDelete = this.handleDelete.bind(this)
-    this.handleCheck = this.handleCheck.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)  // Bind des événements vers les components
+    this.handleCheck = this.handleCheck.bind(this)    
   }
 
+  // Vérification si il y a des données en cache, chargement de ceux-ci si elles existent
   componentWillMount () {
     const storage = JSON.parse(localStorage.getItem('todolist'))
     if (!isNull(storage)){
@@ -23,78 +30,87 @@ class App extends React.Component {
         todoList: storage.todoList
       })
     }
-
-    console.log(storage)
   }
 
+  // Evénement lorsqu'on tape dans le champ de texte
   onChange(event){
     this.setState({
-      userInput: event.target.value
+      userInput: event.target.value // On change l'état de la variable
     })
   }
 
+  // Evénement lorsq'un valide l'ajout d'un événement
   addTodo(event){
-    this.setState({
-      userInput: '',
-      todoIndex: this.state.todoIndex + 1,
-      todoList: [...this.state.todoList, {id: this.state.todoIndex, title: this.state.userInput, checked: false}]
-    }, () => this.saveData(this.state))
-    event.preventDefault()
-    console.log(this.state)
-    
-
+    if (this.state.userInput !== ''){
+      // On modifie l'état de la classe
+      this.setState({
+        userInput: '',                          // On vide le champ de texte
+        todoIndex: this.state.todoIndex + 1,    // On incrémente le compteur d'indexe
+        // On ajoute un nouvel objet au tableau de la liste des tâches avec l'ID, le texte entrée et la valeur "checked" en false par défaut
+        todoList: [...this.state.todoList, {id: this.state.todoIndex, title: this.state.userInput, checked: false}]
+      }, () => this.saveData(this.state))
+      event.preventDefault()    // On empêche la page de se recharger a cause du formulaire
+    } else {
+      event.preventDefault()
+    }
   }
 
+  // Fonction qui va lire la liste et les afficher sur la page avec un map
   listTodo(){
     return this.state.todoList.map((item) => {
       return(
         <TodoItem key={item.id} onCheck={this.handleCheck} onDelete={this.handleDelete} task={ item }/>
+        // onCheck et onDelete: va pouvoir communiquer l'évélement depuis le component en envoyant par les props
       )
     })
   }
 
+  // Gestion de l'événement lorsqu'on coche la case depuis le component
   handleCheck(id, state){
-    console.log({state})
-    console.log({id})
-    const array = this.state.todoList
-    const index = array.findIndex(i => i.id === id)
+    let array = this.state.todoList                   // On stocke le tableau de la liste des tâche dans une variable
+    const index = array.findIndex(i => i.id === id)   // On cherche dans quel index se trouve notre ID concernée
 
-    array[index].checked = state
+    array[index].checked = state                      // On applique le changement de variable dans notre tableau
 
-    this.saveData(this.state)
+    this.setState({ 
+      todoList: array                                 // On applique les changement faites au tableau dans la state
+    }, () => this.saveData(this.state))               // On sauvegarde les changements aux données locale
   }
-
+  
+  // Gestion de l'événement lorsqu'on supprime une tâche
   handleDelete(id){
-    const array = this.state.todoList
-    const index = array.findIndex(i => i.id === id)
+    let array = this.state.todoList                   // On stocke le tableau de la liste des tâche dans une variable
+    const index = array.findIndex(i => i.id === id)   // On cherche dans quel index se trouve notre ID concernée
 
-    array.splice(index, 1)
+    array.splice(index, 1)                            // On supprime l'élément du tableau concerné
 
     this.setState({
-      todoList: array
-    }, () => this.saveData(this.state))
+      todoList: array                                 // On applique les changement faites au tableau dans la state
+    }, () => this.saveData(this.state))               // On sauvegarde les changements aux données locale
 
     
   }
 
+  // Fonction pour sauvegarder les données localement
   saveData(data){
     localStorage.setItem('todolist', JSON.stringify(data))
   }
 
+  // Rendu de l'application
   render(){
     return (
       <div className="wrapper">
         <h1>Todo list</h1>
         
         <div className="todo-list">
-          {this.listTodo()}
+          {this.listTodo()} {/* Appelle la fonction qui liste la liste des tâche dans la page*/}
           <form className="todo-item">
             <button onClick={this.addTodo.bind(this)}><span role="img" aria-label="add">➕</span></button>
             <input 
-              value={this.state.userInput}
+              value={this.state.userInput}          // Reprend la valeur de l'état userInput
               type='text' 
               placeholder='Ajouter une tâche'
-              onChange={this.onChange.bind(this)}
+              onChange={this.onChange.bind(this)}   // Communique avec l'événement onChange qui change l'état userInput
               className="todo-input"
             />
           
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,13 +4,13 @@ class TodoItem extends React.Component{
     constructor(props) {
         super(props)
         this.state={
-            checked: this.props.task.checked
+            checked: this.props.task.checked    // Reprend l'état check reçu en props
         }
     }
 
     checkItem = () => {
-        let checked = ''
-        if (!this.state.checked){
+        let checked = ''                        // Création d'une variable check à part
+        if (!this.state.checked){               // Changement de l'état check
             this.setState({checked: true})
             checked = true
         } else {
@@ -18,16 +18,18 @@ class TodoItem extends React.Component{
             checked = false
         }
 
-        this.props.onCheck(this.props.task.id, checked)
+        this.props.onCheck(this.props.task.id, checked) // Change l'état check dans la variable props
     }
 
     render(){
         return(
             <div className='todo-item'>
+              {/*                         Appelle la fonction pour changer l'état                       Affiche soit la case verte ou rouge selon l'état */}
                 <button className='check' onClick={this.checkItem}><span role="img" aria-label="check">{this.state.checked ? "✅" : "🟥"}</span></button> { this.props.task.title }
+              {/*                               Appelle l'événement sur app.js avec l'ID en tant qu'argument */}
                 <button className='todo-remove' onClick={this.props.onDelete.bind(this, this.props.task.id)}><span role="img" aria-label="remove">❌</span></button>
             </div>
-        )
+        );
     }
 }
 
